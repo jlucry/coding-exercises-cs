15e3edab787cafcc8d13ee15a1b089a18d8ca49c
Jean-Olivier LUCRY <jlucry@yahoo.com>
2020-10-22 16:00:17 +0200

    add coding question in class summary

--- src/array/FindDuplicateInteger.cs
+++ src/array/FindDuplicateInteger.cs
@@ -1,9 +1,13 @@
 using System;
 
 namespace array
-{
-
-    // Find a duplicate from an array of n integers. Integers are between 0 and n-1.
+{    
+    /// <summary>
+    /// (6) An array contains n numbers ranging from 0 and n-1. There are some numbers duplicated in the array.
+    /// It is not clear how many numbers are duplicated or many times a number gets duplicated.
+    /// How do you find a duplicated number in the array? For example if an array of length 7 contains the numbers {2,3,1,0,2,5,3}
+    /// the implemented function should return either 2 or 3.
+    /// </summary>
     public static class FindDuplicateInterger
     {
         // Use a hashtable to store occurence of the array integers 
